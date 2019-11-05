a32682f325ea48c779db309f10b4bf260026117f
deelocruz <kddaniel8000@gmail.com>
2019-11-05 15:09:23 -0500

    Updates

---
+++ precache-manifest.813a55f47690fdaf741342b2aa05bccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983284308087ab6be0ab715cd65a5fdc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0caff3b2c1607eb3b8ba",
+    "url": "/monsters-rolodex/static/css/main.c8d1e5c3.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "0caff3b2c1607eb3b8ba",
+    "url": "/monsters-rolodex/static/js/main.822c8841.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
