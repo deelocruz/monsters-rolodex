148acabacc779753d47d11043a670b2eaa3ef888
deelocruz <kddaniel8000@gmail.com>
2019-11-05 00:43:31 -0500

    search-box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
 
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -22,19 +24,23 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render(){
 
     const { monsters, searchField } = this.state;  
-    const filteredMonsters = monster.filter(monster => {
+    const filteredMonsters = monsters.filter(monster => 
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
-    });
+    );
+    
     return(
       <div className='App'>
-        <input 
-          type='search' 
-          placeholder='search monsters' 
-          onChange={e => this.setState({ searchField: e.target.value })} 
-          />
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     )
